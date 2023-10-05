25abb72c6c2e00e968161cbe8df1bbef13e43f71
Amanda Caroline <amandacsr10@gmail.com>
2023-10-05 09:52:55 -0300

    exercicio 1 e 2

---
+++ exercicio1/exercicio1.cs
@@ -0,0 +1,7 @@
+int numA, numB, soma;
+numA = 4;
+numB = 5;
+
+soma = numA + numB;
+
+Console.WriteLine("O resultado dessa operação é" + soma)
---
+++ exercicio2/exercicio2.cs
@@ -0,0 +1,16 @@
+int idade = 48; 
+int velocidade = 96;
+
+long valorX = 45L;
+
+float altura;
+altura = 1.98F;
+
+double peso;
+peso = 0.985346;
+
+string nome;
+nome = "Amanda";
+
+bool tentativa;
+tentativa = false;
